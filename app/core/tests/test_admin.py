a140c381a70d2a036cb655c17ad805917b3a9603
Jose <jose@abaqus.cl>
2022-02-01 14:43:12 -0300

    se crea comando wait for db

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -30,6 +30,7 @@ class AdminSiteTests(TestCase):
         """Test that the user edit page works"""
 
         url = reverse("admin:core_user_change", args=[self.user.id])
+        # /admin/core/user/id_user
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
